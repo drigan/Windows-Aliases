6583e93bcff2715a3e7fb92b980a797cb804ce6d
drigan <driganx@gmail.com>
2014-05-27 08:56:18 -0400

    Fixing error that would only find css directories.

--- go.bat
+++ go.bat
@@ -1,3 +1,6 @@
+:: Quick script to search subdirectories and cd to the first on that matches a passed in variable.
 @echo off
-for /d /r "." %%a in (*) do if /i "%%~nxa"=="css" set "folderpath=%%a"
+set foldertofind=%1
+set folderpath=%CD%
+for /d /r "." %%a in (*) do if /i "%%~nxa"=="%foldertofind%" set "folderpath=%%a"
 cd /d %folderpath%
