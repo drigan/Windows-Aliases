bf311de7b827f548c6feb4434901b41242567fec
Joe Landrigan <driganx@gmail.com>
2014-07-09 10:34:19 -0400

    Several small updates.

--- go.bat
+++ go.bat
@@ -2,5 +2,5 @@
 @echo off
 set foldertofind=%1
 set folderpath=%CD%
-for /d /r "." %%a in (*) do if /i "%%~nxa"=="%foldertofind%" set "folderpath=%%a"
+for /d /r "." %%a in (*) do if /i "%%~nxa"=="%foldertofind%" if /i not "%folderpath%~nxa"=="%foldertofind%" set "folderpath=%%a"
 cd /d %folderpath%
